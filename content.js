6a6078e125fab4e1e6a6f5949ecb6f870c123dee
rushil-patel <rushilpatel10@gmail.com>
2015-10-25 16:32:03 -0700

    refactored
    removed unused code

--- content.js
+++ content.js
@@ -1,18 +1,5 @@
+//Rushil Patel
 //content.js
-
-//listen for message from background script(s)
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    if( request.message === "clicked_browser_action" ) {
-      var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-      console.log(firstHref);
-
-      //send message to background script(s)
-      chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
-    }
-  }
-);
 
 var dataDict = {};
 
